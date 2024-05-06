626045f3bbfdbd4c12e6f02a58e8eb5995741a72
Jan Trzicky <jan@sionzee.cz>
2024-05-06 22:10:24 +0200

    chore: distinguish owner messages

--- lib/ui/channel_message.dart
+++ lib/ui/channel_message.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:talk/core/models/models.dart' as models;
+import 'package:talk/core/notifiers/current_connection.dart';
 import 'package:talk/core/notifiers/theme_controller.dart';
 import 'package:talk/ui/user_avatar.dart';
 
@@ -28,6 +29,25 @@ class ChannelMessage extends StatefulWidget {
 class ChannelMessageState extends State<ChannelMessage> {
   bool _isHovered = false;
 
+  getBackgroundColor() {
+    // if is current user
+    if (widget.user?.id == CurrentSession().user?.id) {
+      if (_isHovered) {
+        return ThemeController
+            .scheme(context)
+            .surfaceContainerLowest;
+      } else {
+        return ThemeController.scheme(context).surfaceContainerLow;
+      }
+    }
+
+    if(_isHovered) {
+      return ThemeController.scheme(context).surfaceContainerLowest;
+    } else {
+      return Colors.transparent;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final colorScheme = ThemeController.scheme(context);
@@ -36,54 +56,62 @@ class ChannelMessageState extends State<ChannelMessage> {
       onExit: (_) => setState(() => _isHovered = false),
       child: Container(
         padding: const EdgeInsets.all(8),
+        margin: const EdgeInsets.fromLTRB(0, 1, 0, 1),
         decoration: BoxDecoration(
-          border: Border.all(
-            color: _isHovered ? Colors.grey : Colors.transparent,
-          ),
+          color: getBackgroundColor(),
+          borderRadius: BorderRadius.circular(5),
         ),
-        child: Stack(
-          children: [
-            Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              UserAvatar(imageUrl: widget.user?.avatar),
-              const SizedBox(width: 8),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        Text(widget.user?.displayName ?? '<user not found>', style: TextStyle(color: colorScheme.onSurface, fontWeight: FontWeight.bold),),
-                        const SizedBox(width: 8),
-                        if(widget.message.createdAt != null) Text(DateFormat('HH:mm:ss').format(DateTime.parse(widget.message.createdAt)), style: TextStyle(color: colorScheme.onSurface, fontStyle: FontStyle.italic)),
-                        if (widget.message.createdAt == null) Text(widget.message.createdAt ?? '<No time>', style: TextStyle(color: colorScheme.onSurface, fontStyle: FontStyle.italic)),
-                      ],
-                    ),
-                    Text(widget.message.content ?? '<No message>', style: TextStyle(color: colorScheme.onSurface)),
-                  ],
-                ),
-              ),
-            ],
+        child: Theme(
+          data: Theme.of(context).copyWith(
+            textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: colorScheme.onSurface,
+              displayColor: colorScheme.onSurface,
+            ),
           ),
-          if (_isHovered)
-              Positioned(
-                bottom: 0,
-                right: 0,
-                child: Row(
-                  children: [
-                    IconButton(
-                      icon: const Icon(Icons.edit),
-                      onPressed: widget.onEdit,
-                    ),
-                    IconButton(
-                      icon: const Icon(Icons.delete),
-                      onPressed: widget.onDelete,
-                    ),
-                  ],
+          child: Stack(
+            children: [
+              Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                UserAvatar(imageUrl: widget.user?.avatar),
+                const SizedBox(width: 8),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Text(widget.user?.displayName ?? '<user not found>', style: TextStyle(color: colorScheme.onSurface, fontWeight: FontWeight.bold),),
+                          const SizedBox(width: 8),
+                          if(widget.message.createdAt != null) Text(DateFormat('HH:mm:ss').format(DateTime.parse(widget.message.createdAt)), style: TextStyle(color: colorScheme.onSurface, fontStyle: FontStyle.italic)),
+                          if (widget.message.createdAt == null) Text(widget.message.createdAt ?? '<No time>', style: TextStyle(color: colorScheme.onSurface, fontStyle: FontStyle.italic)),
+                        ],
+                      ),
+                      Text(widget.message.content ?? '<No message>', style: TextStyle(color: colorScheme.onSurface)),
+                    ],
+                  ),
                 ),
-              ),
-          ]
+              ],
+            ),
+            if (_isHovered)
+                Positioned(
+                  bottom: 0,
+                  right: 0,
+                  child: Row(
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.edit),
+                        onPressed: widget.onEdit,
+                      ),
+                      IconButton(
+                        icon: const Icon(Icons.delete),
+                        onPressed: widget.onDelete,
+                      ),
+                    ],
+                  ),
+                ),
+            ]
+          ),
         ),
       ),
     );
