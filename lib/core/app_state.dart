d7606f5190f29cb16de3c6b38147af469ed97922
Jan Trzicky <jan@sionzee.cz>
2024-09-24 00:18:02 +0200

    chore: bump version

--- lib/core/app_state.dart
+++ lib/core/app_state.dart
@@ -1,5 +1,5 @@
 class AppState {
-  static const String version = '0.0.14';
+  static const String version = '0.0.15';
   static const String appName = 'Mickle';
   static bool overSplash = false;
 }
