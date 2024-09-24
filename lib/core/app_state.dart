e750289a6c4c8514a34aa4763605079e0f59d4f7
Jan Trzicky <jan@sionzee.cz>
2024-09-24 02:39:39 +0200

    chore: bump pubspec version

--- lib/core/app_state.dart
+++ lib/core/app_state.dart
@@ -1,4 +1,5 @@
 class AppState {
+  // Do not forget to update the version in pubspec.yaml
   static const String version = '0.0.15';
   static const String appName = 'Mickle';
   static bool overSplash = false;
